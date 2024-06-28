acb156d700c274bc78bb6d7462140d5ab8e2b879
Group011 <Group011@infoway.com>
2024-06-28 17:55:28 +0530

    DataBase Connectivity Using DotNet Done

---
+++ DisConnectedExamples/AssignmentOnDisconnectedArchitecture/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AssignmentOnDisconnectedArchitecture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMoveLast_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMoveNext_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMovePreveious_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMoveFirst_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcontactname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contactname_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void city_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtcustomerId_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void customerId_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
